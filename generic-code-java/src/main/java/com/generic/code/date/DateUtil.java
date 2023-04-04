238593d87bddd4fd39edc9da3d1029f4de222f6a
Ryo H <49798519+ryobs@users.noreply.github.com>
2023-04-04 20:20:23 +0900

    Create DateUtil

---
+++ generic-code-java/src/main/java/com/generic/code/date/DateUtil.java
@@ -0,0 +1,116 @@
+package com.generic.code.date;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Utility class for using {@link java.util.Calendar} and {@link java.util.Date} object.
+ */
+public class DateUtil {
+
+  /**
+   * constructor.
+   */
+  private DateUtil() {}
+
+  /**
+   * Obtains an instance of {@code Date} from year, month and day, setting the hour, minute, second
+   * and millisecond to zero.
+   *
+   * @param year the year to represent
+   * @param month the month-of-year to represent, from 1 (January) to 12 (December)
+   * @param dayOfMonth the day-of-month to represent, from 1 to 31
+   * @return an instance of {@code Date}
+   */
+  public static Date createDate(int year, int month, int dayOfMonth) {
+    return createCalendar(year, month, dayOfMonth).getTime();
+  }
+
+  /**
+   * Obtains an instance of {@code Date} from year, month,day, hour, minute and second, setting the
+   * millisecond to zero.
+   *
+   * @param year the year to represent
+   * @param month the month-of-year to represent, from 1 (January) to 12 (December)
+   * @param dayOfMonth the day-of-month to represent, from 1 to 31
+   * @param hour the hour-of-day to represent, from 0 to 23
+   * @param minute the minute-of-hour to represent, from 0 to 59
+   * @param second the second-of-minute to represent, from 0 to 59
+   * @return an instance of {@code Date}
+   */
+  public static Date createDate(int year, int month, int dayOfMonth, int hour, int minute,
+      int second) {
+    return createCalendar(year, month, dayOfMonth, hour, minute, second).getTime();
+  }
+
+  /**
+   * Obtains an instance of {@code Date} from year, month, day, hour, minute, second and
+   * millisecond.
+   *
+   * @param year the year to represent
+   * @param month the month-of-year to represent, from 1 (January) to 12 (December)
+   * @param dayOfMonth the day-of-month to represent, from 1 to 31
+   * @param hour the hour-of-day to represent, from 0 to 23
+   * @param minute the minute-of-hour to represent, from 0 to 59
+   * @param second the second-of-minute to represent, from 0 to 59
+   * @param millisecond the milli-of-second to represent, from 0 to 999
+   * @return an instance of {@code Date}
+   */
+  public static Date createDate(int year, int month, int dayOfMonth, int hour, int minute,
+      int second, int millisecond) {
+    return createCalendar(year, month, dayOfMonth, hour, minute, second, millisecond).getTime();
+  }
+
+  /**
+   * Obtains an instance of {@code Calendar} from year, month and day, setting the hour, minute,
+   * second and millisecond to zero.
+   *
+   * @param year the year to represent
+   * @param month the month-of-year to represent, from 1 (January) to 12 (December)
+   * @param dayOfMonth the day-of-month to represent, from 1 to 31
+   * @return an instance of {@code Calendar}
+   */
+  public static Calendar createCalendar(int year, int month, int dayOfMonth) {
+    return createCalendar(year, month, dayOfMonth, 0, 0, 0, 0);
+  }
+
+  /**
+   * Obtains an instance of {@code Calendar} from year, month,day, hour, minute and second, setting
+   * the millisecond to zero.
+   *
+   * @param year the year to represent
+   * @param month the month-of-year to represent, from 1 (January) to 12 (December)
+   * @param dayOfMonth the day-of-month to represent, from 1 to 31
+   * @param hour the hour-of-day to represent, from 0 to 23
+   * @param minute the minute-of-hour to represent, from 0 to 59
+   * @param second the second-of-minute to represent, from 0 to 59
+   * @return an instance of {@code Calendar}
+   */
+  public static Calendar createCalendar(int year, int month, int dayOfMonth, int hour, int minute,
+      int second) {
+    return createCalendar(year, month, dayOfMonth, hour, minute, second, 0);
+  }
+
+  /**
+   * Obtains an instance of {@code Calendar} from year, month, day, hour, minute, second and
+   * millisecond.
+   *
+   * @param year the year to represent
+   * @param month the month-of-year to represent, from 1 (January) to 12 (December)
+   * @param dayOfMonth the day-of-month to represent, from 1 to 31
+   * @param hour the hour-of-day to represent, from 0 to 23
+   * @param minute the minute-of-hour to represent, from 0 to 59
+   * @param second the second-of-minute to represent, from 0 to 59
+   * @param millisecond the milli-of-second to represent, from 0 to 999
+   * @return an instance of {@code Calendar}
+   */
+  public static Calendar createCalendar(int year, int month, int dayOfMonth, int hour, int minute,
+      int second, int millisecond) {
+    final Calendar c = Calendar.getInstance();
+    c.clear();
+    c.setLenient(false);
+    c.set(year, (month - 1), dayOfMonth, hour, minute, second);
+    c.set(Calendar.MILLISECOND, millisecond);
+    return c;
+  }
+}
