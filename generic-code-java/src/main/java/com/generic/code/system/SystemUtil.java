9d67f2572681b083519b5b7f0f8cbc49764c83fe
Ryo H <49798519+ryobs@users.noreply.github.com>
2023-04-17 19:52:32 +0900

    Add SystemUtil.java

---
+++ generic-code-java/src/main/java/com/generic/code/system/SystemUtil.java
@@ -0,0 +1,21 @@
+package com.generic.code.system;
+
+/**
+ * Utility class for using {@link java.lang.System}.
+ */
+public class SystemUtil {
+
+  /**
+   * constructor.
+   */
+  private SystemUtil() {}
+
+  /**
+   * Prints an object.
+   *
+   * @param x the Object to be printed.
+   */
+  public static void print(Object x) {
+    System.out.println(x);
+  }
+}
---
+++ generic-code-java/src/main/java/com/generic/code/system/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This package contains a collection of codes related to system.
+ */
+package com.generic.code.system;
