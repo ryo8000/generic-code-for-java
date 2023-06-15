5cc4bf02a24642874e3ee08a7f9467969382235b
Ryo H <49798519+ryobs@users.noreply.github.com>
2023-06-16 08:11:04 +0900

    Remove unnecessary throws.

--- src/test/java/com/generic/code/base/StringUtilTest.java
+++ src/test/java/com/generic/code/base/StringUtilTest.java
@@ -2,7 +2,6 @@ package com.generic.code.base;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
-import java.text.ParseException;
 import org.junit.Test;
 
 /**
@@ -10,19 +9,19 @@ import org.junit.Test;
  */
 public class StringUtilTest {
   @Test(expected = NullPointerException.class)
-  public void testPadLeftNull() throws ParseException {
+  public void testPadLeftNull() {
     StringUtil.padLeft(null, 10, 'x');
   }
 
   @Test
-  public void testPadLeftEmpty() throws ParseException {
+  public void testPadLeftEmpty() {
     assertThat("", is(StringUtil.padLeft("", 0, ' ')));
     assertThat(" ", is(StringUtil.padLeft("", 1, ' ')));
     assertThat("  ", is(StringUtil.padLeft("", 2, ' ')));
   }
 
   @Test
-  public void testPadLeftString() throws ParseException {
+  public void testPadLeftString() {
     assertThat("1", is(StringUtil.padLeft("1", 0, '0')));
     assertThat("1", is(StringUtil.padLeft("1", 1, '0')));
     assertThat("01", is(StringUtil.padLeft("1", 2, '0')));
@@ -34,7 +33,7 @@ public class StringUtilTest {
   }
 
   @Test
-  public void testPadLeftMinusLength() throws ParseException {
+  public void testPadLeftMinusLength() {
     assertThat("a", is(StringUtil.padLeft("a", -5, 'x')));
   }
 }
