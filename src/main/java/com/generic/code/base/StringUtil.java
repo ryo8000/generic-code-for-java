62f673119671b7a6060c18011674fe7300b3d69e
Ryo Hasegawa <49798519+ryo8000@users.noreply.github.com>
2024-09-01 11:45:34 +0900

    Moved split method to CollectionsUtil.

--- src/main/java/com/generic/code/base/StringUtil.java
+++ src/main/java/com/generic/code/base/StringUtil.java
@@ -1,8 +1,5 @@
 package com.generic.code.base;
 
-import java.util.Arrays;
-import java.util.List;
-
 /**
  * Utility class related to string.
  */
@@ -34,37 +31,5 @@ public class StringUtil {
     }
     sb.append(target);
     return sb.toString();
-  }
-
-  /**
-   * Splits this string around matches of the given regular expression.
-   *
-   * <p>Returns an empty list if the string to be split is an empty string.
-   *
-   * @param str the string to be split
-   * @param delimiter the delimiting regular expression
-   * @return the list of strings computed by splitting this string around matches of the given
-   *         regular expression
-   */
-  public static List<String> split(String str, String delimiter) {
-    return split(str, delimiter, -1);
-  }
-
-  /**
-   * Splits this string around matches of the given regular expression.
-   *
-   * <p>Returns an empty list if the string to be split is an empty string.
-   *
-   * @param str the string to be split
-   * @param delimiter the delimiting regular expression
-   * @param limit the result threshold, as described above
-   * @return the list of strings computed by splitting this string around matches of the given
-   *         regular expression
-   */
-  public static List<String> split(String str, String delimiter, int limit) {
-    if (str.isEmpty()) {
-      return Arrays.asList();
-    }
-    return Arrays.asList(str.split(delimiter, limit));
   }
 }
---
+++ src/main/java/com/generic/code/collections/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This package contains a collection of codes related to collections.
+ */
+package com.generic.code.collections;
