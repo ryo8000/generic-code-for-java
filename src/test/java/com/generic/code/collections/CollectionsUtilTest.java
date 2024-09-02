aee9931f9d056f99a5c7da03b73094ffccaaea93
Ryo Hasegawa <49798519+ryo8000@users.noreply.github.com>
2024-09-02 21:24:51 +0900

    Add CollectionsUitlTest.java

---
+++ src/test/java/com/generic/code/collections/CollectionsUtilTest.java
@@ -0,0 +1,45 @@
+package com.generic.code.collections;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Test;
+
+/**
+ * Test class for {@link CollectionsUtil}.
+ */
+public class CollectionsUtilTest {
+  @Test(expected = NullPointerException.class)
+  public void testSplitWithNull() {
+    CollectionsUtil.split(null, ",");
+  }
+
+  @Test
+  public void testSplitWithEmptyString() {
+    List<String> expected = Arrays.asList();
+    List<String> actual = CollectionsUtil.split("", ",");
+    assertThat(actual, is(expected));
+  }
+
+  @Test
+  public void testSplitWithoutLimit() {
+    List<String> expected = Arrays.asList("a", "b", "", "c", "", "");
+    List<String> actual = CollectionsUtil.split("a,b,,c,,", ",");
+    assertThat(actual, is(expected));
+  }
+
+  @Test
+  public void testSplitWithLimit() {
+    List<String> expected = Arrays.asList("a", "b", "", "c,,");
+    List<String> actual = CollectionsUtil.split("a,b,,c,,", ",", 4);
+    assertThat(actual, is(expected));
+  }
+
+  @Test
+  public void testSplitWithNoMatchDelimiter() {
+    List<String> expected = Arrays.asList("a,b,c");
+    List<String> actual = CollectionsUtil.split("a,b,c", ";");
+    assertThat(actual, is(expected));
+  }
+}
